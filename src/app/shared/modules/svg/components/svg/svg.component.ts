58081f2d995a3ad350621c9ae89b7415641e8bbf
Raphael Jancherov <bokettodev@gmail.com>
2022-04-09 00:06:28 +0300

    feat(svg): svg component implemented

--- src/app/shared/modules/svg/components/svg/svg.component.ts
+++ src/app/shared/modules/svg/components/svg/svg.component.ts
@@ -1,27 +1,53 @@
 import {
-  Attribute,
   ChangeDetectionStrategy,
   Component,
+  ElementRef,
+  HostBinding,
   Input,
+  OnChanges,
+  SimpleChanges,
 } from '@angular/core';
+import { DomService, HttpService } from '@shared/services';
 
 @Component({
   selector: 'fb-svg',
-  template: `
-    <!-- <svg-icon
-      src="assets/images/{{ src }}.svg"
-      [svgStyle]="{ color, height, width }"
-    ></svg-icon> -->
-  `,
+  template: ``,
   styleUrls: ['./svg.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SvgComponent {
-  @Input() src?: string;
+export class SvgComponent implements OnChanges {
+  @Input() @HostBinding('style.--color') color?: string;
+  @Input() @HostBinding('style.--width') width?: string;
+  @Input() @HostBinding('style.--height') height?: string;
+  @Input() icon?: string;
 
   constructor(
-    @Attribute('color') public color: string,
-    @Attribute('height') public height: string,
-    @Attribute('width') public width: string,
+    private elementRef: ElementRef<HTMLElement>,
+    private domService: DomService,
+    private httpService: HttpService,
   ) {}
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['icon']) {
+      this.downloadSvg();
+    }
+  }
+
+  private downloadSvg(): void {
+    if (!this.icon) {
+      return;
+    }
+
+    this.httpService
+      .get<string>({
+        urlHost: this.domService.window.origin,
+        urlPath: `assets/images/svg/${this.icon}.svg`,
+        httpClientOptions: { responseType: 'text' },
+      })
+      .subscribe((svgText) => this.injectSvg(svgText));
+  }
+
+  private injectSvg(svgText: string): void {
+    this.elementRef.nativeElement.innerHTML = svgText;
+  }
 }
